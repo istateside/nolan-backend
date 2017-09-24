a25ef7d223874f6e4c17329f748ca598b8cb35f3
Kevin Fleischman <kevinrfleischman@gmail.com>
2017-09-24 13:52:57 -0400

    sort of working.

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -3,23 +3,40 @@
   theme.name = 'default'
   theme.title = 'Default Theme'
 
-  theme.page_parts = [{
-    name:           'text',
-    title:          'Text',
-    partable_type:  'Spina::Text'
-  }]
+  theme.page_parts = [
+    {
+      name:           'text',
+      title:          'Text',
+      partable_type:  'Spina::Text'
+    }, {
+      name: 'slideshow',
+      title: 'Slideshow',
+      partable_type: 'Spina::PhotoCollection'
+    }, {
+      name: 'cover_image',
+      title: 'Cover Image',
+      partable_type: 'Spina::Photo'
+    }
+  ]
 
-  theme.view_templates = [{
-    name:       'homepage',
-    title:      'Homepage',
-    page_parts: ['text']
-  }, {
-    name: 'show',
-    title:        'Default',
-    description:  'A simple page',
-    usage:        'Use for your content',
-    page_parts:   ['text']
-  }]
+  theme.view_templates = [
+    {
+      name:       'homepage',
+      title:      'Homepage',
+      page_parts: ['text']
+    }, {
+      name:         'show',
+      title:        'Work',
+      description:  'A portfolio page showcasing content in a slideshow',
+      usage:        'Use as collection pages for your work',
+      page_parts:   ['text', 'slideshow', 'cover_image']
+    }, {
+      name: 'about',
+      title: 'About',
+      description: 'A page with contact links and a short bio about you',
+      page_parts: ['text']
+    }
+  ]
 
   theme.custom_pages = [{
     name:           'homepage',
