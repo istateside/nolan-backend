530ecddf35249118808b7ff54ce952de1c2e90be
Kevin Fleischman <kevinrfleischman@gmail.com>
2017-10-15 15:07:06 -0400

    adds instance variables to projects page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,21 @@
 class PagesController < ApplicationController
   def home
+
   end
 
   def other
+    @projects = Project.where(type: "Other")
   end
 
   def comics
+    @projects = Project.where(type: "Comics")
   end
 
   def illustrations
+    @projects = Project.where(type: "Illustrations")
   end
 
   def about
+
   end
 end
