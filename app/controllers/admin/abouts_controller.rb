464fef09b749050100798ae7048b5fb6f23f73d0
Kevin Fleischman <kevinrfleischman@gmail.com>
2017-10-15 21:16:00 -0400

    Sets up admin pages to manage projects phew

--- app/controllers/admin/abouts_controller.rb
+++ app/controllers/admin/abouts_controller.rb
@@ -18,9 +18,10 @@ class Admin::AboutsController < AdminController
   private
   def set_about
     @about = Project.where(category: "About").first_or_create!
+    @cover_image = @about.cover_image || @about.cover_image.build
   end
 
   def about_params
-    params.require(:project).permit(:cover_image, :description)
+    params.require(:project).permit(:description, { cover_image_attributes: [:attachment] })
   end
 end
--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,22 +1,69 @@
 class Admin::ProjectsController < AdminController
   def index
+    if params[:category]
+      @projects = Project.where(category: params[:category])
+    else
+      @projects = Project.all
+    end
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      redirect_to admin_project_url @project
+    else
+      render :edit
+    end
   end
 
   def create
+    @project = Project.find(params[:id])
+
+    if @project.create(project_params)
+      redirect_to admin_project_url @project
+    else
+      render :new
+    end
   end
 
   def new
+    @project = Project.new
   end
 
   def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to admin_projects_url
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(
+      :title,
+      :category,
+      :description,
+      {
+        cover_image_attributes: [:attachment, :id, :_destroy]
+      },
+      {
+        slides_attributes: [
+          :description,
+          :id,
+          :_destroy,
+          {
+            image_attributes: [:id, :attachment]
+          }
+        ]
+      }
+    )
   end
 end
